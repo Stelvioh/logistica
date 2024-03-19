2a702697ccbec904198abee763b49ed7d671b1e5
Stelvioh <stelvioh@gmail.com>
2024-03-19 17:23:25 -0300

    Busca de usuarios por faixa de idade

--- src/main/java/com/casa/logistica/repository/UsuarioRepository.java
+++ src/main/java/com/casa/logistica/repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.casa.logistica.repository;
 
 import com.casa.logistica.domain.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 // Aqui estamos criando um repositório para a entidade Usuario utilizando crudRepository
@@ -15,5 +17,8 @@ public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
     //Buscar todos os usuarios
     List<Usuario> findAll();
 
+    // Query que busca usuários pela faixa de idade usando SQL
+    @Query("SELECT u FROM Usuario u WHERE u.dataNascimento BETWEEN :startDate AND :endDate")
+    List<Usuario> findUsersByAgeRange(LocalDate startDate, LocalDate endDate);
 }
 
